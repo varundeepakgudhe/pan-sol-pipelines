30d7b62e390ee90f26dc84304afe9788bfa1c314
Varun Deepak Gudhe <112991463+varundeepakgudhe@users.noreply.github.com>
2025-03-31 12:11:22 -0400

    Update run_genespace_order.R

--- gene_annotation/GENESPACE/run_genespace_order.R
+++ gene_annotation/GENESPACE/run_genespace_order.R
@@ -29,12 +29,12 @@ print("Checking files in run_dir")
 print(list.files(paste0(runwd,"/rawGenomes"), recursive = T, full.names = F))
 annotation_dirs<-unique(lapply(list.files(paste0(runwd,"/rawGenomes"), recursive = T, full.names = F),dirname))
 genome_dirs<-lapply(annotation_dirs,dirname)
-#gids<-lapply(genome_dirs,basename)
+
 gids<-c("SlycopersicumHeinz","SlycopersicumM82","SlycopersicumSweet100","Smuricatumhap1","Smuricatumhap2","Sabutiloides", "Squitoense","Sprinophyllum","Smacrocarpon","Saethiopicum","Smelongena41")
 
 print(paste0("Set gene ids: ",gids))
 #3.3 Initialize the GENESPACE run
-#gids <- c("human","chimp","rhesus")
+
 print("Initializing GENESPACE")
 ploidy=rep(PLOIDY,length(gids))
 gpar <- init_genespace(
@@ -48,7 +48,6 @@ gpar <- init_genespace(
   path2orthofinder = "orthofinder", 
   path2mcscanx = file.path("/grid/lippman/data/pansol/sources/MCScanX/"),
   rawGenomeDir = file.path(runwd,"rawGenomes"))
-  #rawGenomeDir = file.path(runwd))
 
 print("Initiolization Complete \n")
 print("Parse annotations")
@@ -61,53 +60,18 @@ parse_annotations(
   headerSep = " ", 
   headerStripText = "ID=")
 
-#parse_annotations(
-#  gsParam = gpar,
-#  gffEntryType = "gene",
-#  gffIdColumn = "locus",
-#  gffStripText = "locus=",
-#  headerEntryIndex = 1,
-#  headerSep = " ",
-#  headerStripText = "locus=")
-
 print("Running orthofinder")
 gpar <- run_orthofinder(gsParam = gpar)
 
 print("Running synteny")
 gpar <- synteny(gsParam = gpar)
 refGenome="SlycopersicumHeinz"
-#invert_chr<- data.frame(genome="S.melongena", chr="chr2")
-#invert_chr[2,] <- c("Smelongena", "chr3")
-#invert_chr[3,] <- c("Smelongena", "chr8")
-#invert_chr[4,] <- c("Smelongena", "chr9")
-#invert_chr[4,] <- c("Smelongena", "chr10")
-
-#invert_chr<- data.frame(genome="Smelongena", chr="chr3")
-#invert_chr[2,] <- c("Smelongena","chr8")
-#invert_chr[3,] <- c("Smelongena", "chr9")
-#invert_chr[4,] <- c("Smelongena", "chr10")
-#invert_chr[5,] <- c("Saethiopicum", "chr1")
-#invert_chr[6,] <- c("Saethiopicum","chr2")
-#invert_chr[7,] <- c("Smacrocarpon", "chr1")
-#invert_chr[8,] <- c("Sprinophyllum", "chr1")
-#invert_chr[9,] <- c("Squitoenese", "chr1")
-#invert_chr[10,] <- c("Squitoenese", "chr2")
-#invert_chr[11,] <- c("Squitoenese", "chr12")
-#invert_chr[12,] <- c("Squitoenese", "chr10")
-#invert_chr[13,] <- c("Smuricatumhap2", "chr9") 
-
 
 print("Plotting Riparian hits")
 pdf(file.path(paste0(runwd,"/results/pangenome_ripariani_order.pdf")))
 ripdat <- plot_riparianHits(gpar,useBlks = TRUE, refGenome = refGenome , useOrder = TRUE, reorderChrs = TRUE, returnSourceData = T)
-#ripdat <- plot_riparianHits(gpar)
+
 dev.off()
-
-#pdf(file.path(paste0(runwd,"/results/pangenome_riparian_invert.pdf")))
-#ripdat <- plot_riparianHits(gpar,useBlks = TRUE, useOrder = TRUE , refGenome = refGenome , returnSourceData = T)
-#ripdat <- plot_riparianHits(gpar,useBlks = TRUE, refGenome = refGenome , useOrder = TRUE, reorderChrs = TRUE, invertTheseChrs = invert_chr, returnSourceData = T)
-#dev.off()
-
 
 print("Generating pangenome")
 pg <- pangenome(gpar)
